200f9530409bad22e78e2a35dbfd9cc9f75399ef
leosefcik <leosefcik@gmail.com>
2020-12-14 10:53:22 +0100

    Update moňáze.py
    v1.2

--- moňáze.py
+++ moňáze.py
@@ -29,13 +29,17 @@ while True:
         print("You haven't bought/sold anything yet!")
     else:
         print("You have gained/lost",(paid-paid*2)/1000,"money for",', '.join(items))
+    if money/1000<0:
+        negativ=1
+    else:
+        negativ=0
     yesno=input("Would you like to buy/sell anything? (y/n): ")
     print(" ")
     if yesno=="y":
         name=input("What are you buying/selling: ")
         cost=float(input("How much does it cost (positive for buying, negative for selling): "))
-        if money-cost*1000<0:
-            print("This costs more money than you have! Are you sure you want to proceed?")
+        if (negativ==0)&(money-cost*1000<0):
+            print("\nThis costs more money than you have! Are you sure you want to proceed?")
             yesno=input("Type y to confirm this item: ")
             if yesno=="y":
                 paid=paid+cost*1000
@@ -95,27 +99,4 @@ for x in range(0,len(items)):
     f.write(" money")
 paus=input("Press Enter to close the program")
 
-#This is free and unencumbered software released into the public domain.
-#
-#Anyone is free to copy, modify, publish, use, compile, sell, or
-#distribute this software, either in source code form or as a compiled
-#binary, for any purpose, commercial or non-commercial, and by any
-#means.
-#
-#In jurisdictions that recognize copyright laws, the author or authors
-#of this software dedicate any and all copyright interest in the
-#software to the public domain. We make this dedication for the benefit
-#of the public at large and to the detriment of our heirs and
-#successors. We intend this dedication to be an overt act of
-#relinquishment in perpetuity of all present and future rights to this
-#software under copyright law.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-#IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-#OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-#ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-#OTHER DEALINGS IN THE SOFTWARE.
-#
-#For more information, please refer to <https://unlicense.org>
+#wahat doses uncucumber d mean tho idk
